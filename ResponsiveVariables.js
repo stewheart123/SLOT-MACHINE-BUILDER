6739740ae151efaf05b2d34612640a2203b2e6fd
Stewart <44583600+stewheart123@users.noreply.github.com>
2023-05-18 10:08:39 +0100

    responsive screen test almost complete

--- ResponsiveVariables.js
+++ ResponsiveVariables.js
@@ -1,6 +1,6 @@
 /**
  * Use variables here to determine the desktop and mobile sizes
- * 
+ *
  */
 
 console.log(window.innerWidth);
@@ -12,17 +12,32 @@ let gameAreaPanelHeight = window.innerHeight - footerHeight;
 let gameContainerYOffset = 0;
 let titleFontSize = 50;
 let titleYOffset = 100;
-let headerContainerWidth = (window.innerWidth / 2 ) - gameAreaPanelWidth; 
+let headerContainerWidth = window.innerWidth / 2 - gameAreaPanelWidth;
 let gameUIPanelWidth = window.innerWidth / 2;
 
-if(window.innerWidth < 768) {
-    console.log('mobile mode');
-    gameContainerYOffset = 100;
-    footerHeight = 100;
-    gameAreaPanelWidth = window.innerWidth - 40;
-    gameAreaPanelHeight = window.innerHeight - (footerHeight + headerHeight);
+handleResize();
+
+function handleResize() {
+  headerHeight = 100;
+  footerHeight = 100;
+  gameAreaPanelWidth = 600;
+  gameAreaPanelHeight = window.innerHeight - footerHeight;
+  gameContainerYOffset = 0;
+  titleFontSize = 50;
+  titleYOffset = 100;
+  headerContainerWidth = window.innerWidth / 2 - gameAreaPanelWidth;
+  gameUIPanelWidth = window.innerWidth / 2;
+
+  if (window.innerWidth < 768) {
     titleFontSize = 36;
-    titleYOffset = headerHeight / 2;
-    headerContainerWidth = window.innerWidth / 2; 
     gameUIPanelWidth = window.innerWidth;
+  }
+  if (window.innerWidth < 1701) {
+    gameContainerYOffset = 100;
+    gameAreaPanelHeight = window.innerHeight - (footerHeight + headerHeight);
+    headerContainerWidth = window.innerWidth / 2;
+    titleYOffset = headerHeight / 2;
+  }
 }
+
+window.addEventListener("resize", handleResize);
--- slot-machine.js
+++ slot-machine.js
@@ -52,30 +52,31 @@ async function makeGameScreen() {
   // the assets are completely loaded and the promise resolves instantly!
   const loadScreenAssets = await PIXI.Assets.loadBundle("game-screen");
   const maskBackground = new PIXI.Graphics();
-  maskBackground.beginFill(0xff0000);
-  maskBackground.drawRect(0, 0, app.screen.width, app.screen.height);
+  maskBackground.beginFill(0x000000);
+  maskBackground.drawRect(0, 0, app.view.width, app.view.height);
   maskBackground.endFill();
   app.stage.addChild(maskBackground);
 
   // background
   const backgroundImage = new PIXI.Sprite(loadScreenAssets.backgroundImage);
   backgroundImage.anchor.set(0.5);
-  backgroundImage.height = app.screen.height;
+  backgroundImage.height = app.view.height;
+
   let backgroundDynamicWidth = utility.autoRatioWidth(
     loadScreenAssets.backgroundImage.width,
     loadScreenAssets.backgroundImage.height,
-    app.screen.height
+    app.view.height
   );
-  if (backgroundDynamicWidth < app.screen.width) {
-    backgroundImage.width = app.screen.width;
+  if (backgroundDynamicWidth < app.view.width) {
+    backgroundImage.width = app.view.width;
   } else {
     backgroundImage.width = utility.autoRatioWidth(
       loadScreenAssets.backgroundImage.width,
       loadScreenAssets.backgroundImage.height,
-      app.screen.height
+      app.view.height
     );
   }
-  backgroundImage.position.set(app.screen.width / 2, app.screen.height / 2);
+  backgroundImage.position.set(app.view.width / 2, app.view.height / 2);
   maskBackground.addChild(backgroundImage);
   app.stage.addChild(maskBackground);
   loadingContainer.destroy();
@@ -91,27 +92,27 @@ async function makeGameScreen() {
   gameContainer.addChild(backgroundPanel);
   gameContainer.width = gameAreaPanelWidth;
   gameContainer.height = gameAreaPanelHeight;
-  
-  
+
   //start position
   gameContainer.position.x = app.view.width / 2 - gameContainer.width / 2;
   //gameContainer.position.y = gameContainerYOffset;
-  gameContainer.position.y = 0 - (gameContainer.height);
-
+  gameContainer.position.y = 0 - gameContainer.height;
   app.stage.addChild(gameContainer);
 
   //end position
-  const gameContainerEndPosition = new PIXI.Point((app.view.width / 2) - (gameContainer.width / 2),gameContainerYOffset );
+  const gameContainerEndPosition = new PIXI.Point(
+    app.view.width / 2 - gameContainer.width / 2,
+    gameContainerYOffset
+  );
   let gameContainerAnimationComplete = false;
   app.ticker.add(() => {
-  if(!gameContainerAnimationComplete) {
+    if (!gameContainerAnimationComplete) {
+      const distance = Math.sqrt(
+        Math.pow(gameContainer.position.x - gameContainerEndPosition.x, 2) +
+          Math.pow(gameContainer.position.y - gameContainerEndPosition.y, 2)
+      );
 
-    const distance = Math.sqrt(
-      Math.pow(gameContainer.position.x - gameContainerEndPosition.x, 2) +
-      Math.pow(gameContainer.position.y - gameContainerEndPosition.y, 2)
-    );
-
-    const threshold = 1; // Adjust the threshold as needed
+      const threshold = 1; // Adjust the threshold as needed
       if (distance <= threshold) {
         // Animation complete, do something here
         console.log("Animation complete!");
@@ -121,16 +122,18 @@ async function makeGameScreen() {
 
       // Move the container towards the target position
       const speed = 0.05; // Adjust the speed of the animation
-      gameContainer.position.x += (gameContainerEndPosition.x - gameContainer.position.x) * speed;
-      gameContainer.position.y += (gameContainerEndPosition.y - gameContainer.position.y) * speed;
-  }
-});
+      gameContainer.position.x +=
+        (gameContainerEndPosition.x - gameContainer.position.x) * speed;
+      gameContainer.position.y +=
+        (gameContainerEndPosition.y - gameContainer.position.y) * speed;
+    }
+  });
 
   // GAME UI
   //balance, time, game name, total bet
   //spin button
   /**
-   *  game menu button > 
+   *  game menu button >
    * pay table = how much you get paid!
    * settings = sound / sound FX / left hand mode
    * Questions - defines all terms mentioned in the game, rules etc
@@ -143,25 +146,31 @@ async function makeGameScreen() {
   //gameUIPanel.pivot.set(0.5);
   gameUIPanel.beginFill(0x000000, 0.5);
   gameUIPanel.lineStyle(2, 0xffffff, 4);
-  gameUIPanel.drawRect(0,0, gameUIPanelWidth, footerHeight);
+  gameUIPanel.drawRect(0, 0, gameUIPanelWidth, footerHeight);
   gameUIPanel.endFill();
   gameUIContainer.addChild(gameUIPanel);
   app.stage.addChild(gameUIContainer);
 
   //start position
-  gameUIContainer.position.set((app.view.width / 2) - (gameUIPanelWidth / 2), app.view.height);
+  gameUIContainer.position.set(
+    app.view.width / 2 - gameUIPanelWidth / 2,
+    app.view.height
+  );
   let gameUIContainerAnimationComplete = false;
   //end position
-  const targetPosition = new PIXI.Point((app.view.width / 2) - (gameUIPanelWidth / 2) , app.view.height - footerHeight);
+  const targetPosition = new PIXI.Point(
+    app.view.width / 2 - gameUIPanelWidth / 2,
+    app.view.height - footerHeight
+  );
   //gameUIContainer.position.set((app.view.width / 2) - (gameUIPanelWidth / 2) , app.view.height - footerHeight);
 
-  //animation 
+  //animation
   app.ticker.add(() => {
     if (!gameUIContainerAnimationComplete) {
       // Calculate the distance between the current position and the target position
       const distance = Math.sqrt(
         Math.pow(gameUIContainer.position.x - targetPosition.x, 2) +
-        Math.pow(gameUIContainer.position.y - targetPosition.y, 2)
+          Math.pow(gameUIContainer.position.y - targetPosition.y, 2)
       );
 
       // Check if the container has reached the target position using a threshold
@@ -176,18 +185,17 @@ async function makeGameScreen() {
 
       // Move the container towards the target position
       const speed = 0.05; // Adjust the speed of the animation
-      gameUIContainer.position.x += (targetPosition.x - gameUIContainer.position.x) * speed;
-      gameUIContainer.position.y += (targetPosition.y - gameUIContainer.position.y) * speed;
+      gameUIContainer.position.x +=
+        (targetPosition.x - gameUIContainer.position.x) * speed;
+      gameUIContainer.position.y +=
+        (targetPosition.y - gameUIContainer.position.y) * speed;
     }
   });
 
   // title
   const headerContainer = new PIXI.Container();
-  // headerContainer.width = app.view.width;
-  // headerContainer.height = headerHeight;
-  //headerContainer.position.set(0, 0);
   app.stage.addChild(headerContainer);
-  
+
   const gameTitle = new PIXI.Text("Slot Machine Builder", {
     fontSize: titleFontSize,
     fill: 0xffffff,
@@ -199,19 +207,54 @@ async function makeGameScreen() {
 
   gameTitle.anchor.set(0.5);
   headerContainer.position.set(headerContainerWidth, titleYOffset);
-  gameTitle.position.set(0,0);
+  gameTitle.position.set(0, 0);
   headerContainer.addChild(gameTitle);
-  
+
   // re-draw items on window resize
   window.addEventListener("resize", plotGraphics);
 
-  
+  // commands to re-draw the containers...
   function plotGraphics() {
-    // commands to re-draw the containers...
+    if (gameContainerAnimationComplete) {
+      app.view.width = window.innerWidth;
+      (app.view.height = window.innerHeight),
+        (backgroundDynamicWidth = utility.autoRatioWidth(
+          loadScreenAssets.backgroundImage.width,
+          loadScreenAssets.backgroundImage.height,
+          app.view.height
+        ));
+      if (backgroundDynamicWidth < app.view.width) {
+        backgroundImage.width = app.view.width;
+      } else {
+        backgroundImage.width = utility.autoRatioWidth(
+          loadScreenAssets.backgroundImage.width,
+          loadScreenAssets.backgroundImage.height,
+          app.view.height
+        );
+      }
+      maskBackground.clear();
+      maskBackground.beginFill(0x000000);
+      maskBackground.drawRect(0, 0, app.view.width, app.view.height);
+      maskBackground.endFill();
+      backgroundImage.position.set(app.view.width / 2, app.view.height / 2);
+      gameContainer.width = gameAreaPanelWidth;
+      gameContainer.height = gameAreaPanelHeight;
+      gameContainer.position.set(
+        app.view.width / 2 - gameContainer.width / 2,
+        gameContainerYOffset
+      );
+      gameUIContainer.width = gameUIPanelWidth;
+      gameUIContainer.height = footerHeight;
+      gameUIContainer.position.set(
+        app.view.width / 2 - gameUIPanelWidth / 2,
+        app.view.height - footerHeight
+      );
+      headerContainer.position.set(headerContainerWidth, titleYOffset);
+    }
   }
 }
 
-// game instruction screen 
+// game instruction screen
 /**
  * - game logo
  * - explain what qualifies as a win
